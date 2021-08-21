58573bad431a52ea2059845c291f09cf78af2e79
Astella Kissan <astellakissan@gmail.com>
2021-08-21 17:35:32 +0300

    dialog beta

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import examplia from '@characters/examplia/examplia';
-import { testroom } from '@backgrounds/bcgIndex';
-import './App.scss';
-import Character from './components/character/character';
+import React, { useState } from "react";
+import examplia from "@characters/examplia/examplia";
+import { testroom } from "@backgrounds/bcgIndex";
+import "./App.scss";
+import Character from "./components/character/character";
+import Dialog, { DialogMessage } from "./components/dialog/dialog";
 
-function App() {
+const App = () => {
+  const [dialog, setDialog] = useState<DialogMessage[]>([
+    {
+      text: "Pamagi",
+      character_name: "Waifu",
+    },
+    {
+      text: "Nyet",
+      character_name: "Semen",
+    },
+  ]);
+
+  const addDialogMessage = (text: string, character_name: string) => {
+    setDialog((curDia) => [...curDia, { text, character_name }]);
+  };
+
+  // let socket = new WebSocket("ws://localhost:4000");
+
+  // socket.onopen = function(e) {
+  //   alert("[open] Connection established");
+  //   alert("Sending to server");
+  //   socket.send("My name is John");
+  // };
+
   return (
     <div className="App">
       <div className="novelWrapper">
         <img src={testroom} className="bcg" />
         <section className="characters">
-          {
-            <Character character={examplia} emotion="testtag" />
-          }
+          {<Character character={examplia} emotion="testtag" />}
         </section>
-        <section className="dialog">
-          <h4 className="charName">Examplia</h4>
-          <p>Bla bla bla</p>
-        </section>
+        <Dialog dialog={dialog} />
+        {/* <h4 className="charName">Examplia</h4> */}
+        {/* <p>Bla bla bla</p> */}
       </div>
-    </div >
+    </div>
   );
-}
+};
 
 export default App;
--- frontend/src/components/dialog/dialog.tsx
+++ frontend/src/components/dialog/dialog.tsx
@@ -1,3 +1,42 @@
+import "./dialog.scss";
 
-const lel = "lelw";
-export { lel }
+export type DialogMessage = {
+  text: string;
+  character_name: string;
+};
+
+type DialogProps = {
+  dialog: DialogMessage[];
+};
+
+const characterColors: Record<string, { clr: string }> = {
+  Waifu: {
+    clr: "#FDBC00",
+  },
+  Semen: {
+    clr: "#3DD643",
+  },
+};
+
+const Dialog = ({ dialog }: DialogProps) => {
+  return (
+    <section className="dialog">
+      {dialog.map(({ character_name, text }, idx) => (
+        <div
+          key={idx}
+          className={`dialogEntry ${character_name === "Waifu" ? "Waifu" : ""}`}
+        >
+          <h4
+            style={{ color: characterColors[character_name].clr }}
+            className="character_name"
+          >
+            {character_name}
+          </h4>
+          <p>{text}</p>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default Dialog;
