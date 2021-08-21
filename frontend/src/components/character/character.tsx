b36776574c18e9c8b354ba9d3f609962226ac22c
Astella Kissan <astellakissan@gmail.com>
2021-08-21 19:43:32 +0300

    examplia great again

--- frontend/src/components/character/character.tsx
+++ frontend/src/components/character/character.tsx
@@ -1,15 +1,20 @@
 import { CharacterT, Emotion } from "@ctypes/character";
 
 type CharacterProps = {
-  position?: "LEFT | CENTER | RIGHT",
-  character: CharacterT,
-  emotion: Emotion
-}
+  position?: "LEFT | CENTER | RIGHT";
+  character: string; // CharacterT;
+  emotion: Emotion;
+};
 
 const Character = ({ position, character, emotion }: CharacterProps) => {
   return (
-    <img src={character[emotion].img} className="character left" />
-  )
-}
+    <img
+      src={
+        /*character[emotion].img */ `/../images/characters/${character}-${emotion}.gif`
+      }
+      className="character left"
+    />
+  );
+};
 
 export default Character;
